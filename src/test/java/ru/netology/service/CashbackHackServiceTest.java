b6e311da13a513d714cc4ed111d671b8655e0ed9
t_klimova@inbox.ru <t_klimova@inbox.ru>
2022-09-22 09:59:34 +0300

    junit4-platform

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology.service;
 
-
 import org.junit.Test;
 import org.junit.Assert;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class CashbackHackServiceTest{
@@ -30,5 +30,29 @@ public class CashbackHackServiceTest{
         int actual = cashbackHackService.remain(1100);
 
         Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainFor900WithAPIJUnitJupiter() {
+        int expected = 100;
+        int actual = cashbackHackService.remain(900);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainFor1000WithAPIJUnitJupiter() {
+        int expected = 0;
+        int actual = cashbackHackService.remain(1000);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainFor1100WithAPIJUnitJupiter() {
+        int expected = 900;
+        int actual = cashbackHackService.remain(1100);
+
+        assertEquals(expected, actual);
     }
 }
